da120556c384fc4f4d859e5a19c9b1f33bdec001
StevBL <stevbl@ip-192-168-1-201.ec2.internal>
2020-12-16 20:01:01 -0600

    feat: First commit, Progress Bar

---
+++ index.js
@@ -0,0 +1,47 @@
+
+
+// global variables
+const container = document.querySelector(".container");
+const containerProg = document.querySelector('.containerPB');
+const btnEnter = document.querySelector('.btnEnter');
+
+
+//style - bg Color
+const body = document.querySelector(".bg");
+body.style.backgroundColor = "#272c34";
+body.style.overflow = "hidden";
+body.style.fontFamily = "Verdana";
+
+function progBarGenerator() {
+    containerProg.innerHTML = `<div class="pb"><div id="progress"></div></div>`;
+  }
+
+function calc(quantity, goal){
+    return (quantity * 100) / goal;
+}
+
+function movePB(quantity, goal) {
+    let adv = Math.ceil(goal/10);
+    let porc = calc(quantity, goal);
+
+    for(let i = 1; i <= adv; i++){
+        let parts = (goal / adv) * i;
+        if(parts <= porc){
+          document.querySelector("#progress").style.width = `${50}%`;
+        }
+    }
+}
+
+function moveOnClick(){
+    let quantity = document.getElementById('quantity').value;
+    let goal = document.getElementById('goal').value;
+
+    if(!isNaN(quantity) && !isNaN(goal)){
+    progBarGenerator();
+    movePB(quantity, goal);
+    }else{
+        alert('Please enter only numeric values.');
+    }
+
+}
+
